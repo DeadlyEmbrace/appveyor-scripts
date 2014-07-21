fbdfc826497275ef34faf0b56dc0b8b61431b2c6
Alex Tercete <tercete@vtex.com.br>
2014-07-21 08:50:38 -0300

    Some more debugging

--- functions/Get-VersionFromGitTag.ps1
+++ functions/Get-VersionFromGitTag.ps1
@@ -1,10 +1,12 @@
 function Get-VersionFromGitTag {
     $tag = Get-GitTagOnLastCommit
-    if ($tag -match "^v(\d+\.\d+\.\d+(-.+)?)$") { $matches[1] } else { $null }
+    if ($tag -match "^v(\d+\.\d+\.\d+(-.+)?)$") {
+        $matches.GetEnumerator() | foreach { Write-Host "$($_.Name)=$($_.Value)" }
+        $matches[1]
+    } else { $null }
 }
 
 function Get-GitTagOnLastCommit {
     ($tag = git describe --tags --exact-match) 2> $null
-    Write-Host "Tag is '$tag'"
     if ($?) { $tag } else { $null }
 }
